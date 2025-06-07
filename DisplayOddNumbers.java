b32fa88048f73323acb3a267c5010afab9bc160e
sai232-cse <saimahitha232@gmail.com>
2025-06-07 19:45:39 +0530

    Update DisplayOddNumbers.java

--- DisplayOddNumbers.java
+++ DisplayOddNumbers.java
@@ -6,6 +6,7 @@ public class DisplayOddNumbers{
         int start=snr.nextInt();
 	System.out.println("Enter end value");
         int end=snr.nextInt();
+	System.out.println("The odd numbers between " + start + " and " + end + "are :")
         for(int i=start;i<=end;i++){
             if(i%2!=0){
                 System.out.print(i+" ");
